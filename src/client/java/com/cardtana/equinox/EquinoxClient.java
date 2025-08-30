932e86302d857cb9c072ac200e0f6d8ea72287f2
firefish <firefish111@protonmail.com>
2025-08-30 08:21:40 +0100

    builds and works

---
+++ src/client/java/com/cardtana/equinox/EquinoxClient.java
@@ -0,0 +1,9 @@
+package com.cardtana.equinox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EquinoxClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
--- src/main/java/com/cardtana/equinox/Equinox.java
+++ src/main/java/com/cardtana/equinox/Equinox.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.cardtana.equinox;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class Equinox implements ModInitializer {
 	public static final String MOD_ID = "modid";
 
 	// This logger is used to write text to the console and the log file.
